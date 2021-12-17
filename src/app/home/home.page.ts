d093df7c9e9559735ab8cb0f056b3113f52031d3
kevin <jhkevin21@gmail.com>
2021-12-17 15:42:34 -0700

    Working on budget planner

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  // providers: [ReactiveFormsModule]
 })
-export class HomePage {
+// export interface resultForm { reason: string; amount: number }
+export class HomePage implements OnInit {
 
-  constructor() {}
+  expenseForm: FormGroup;
+  // // expenseList: []
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.expenseForm = new FormGroup({
+      reason: new FormControl('', Validators.required),
+      amount: new FormControl('', Validators.required)
+    });
+    console.log(this.expenseForm);
+  }
+
+  onSubmit(values) {
+    console.log(values);
+    console.log(this.expenseForm);
+    // this.expenseList.push(values);
+    // this.expenseList = [...this, values]
+  }
+
 
 }
